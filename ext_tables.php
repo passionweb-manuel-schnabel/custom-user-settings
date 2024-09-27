80924ac39587cadc69f892544388c9b71c734e9c
Manuel Schnabel <service@passionweb.de>
2024-09-27 08:58:54 +0200

    initial commit

---
+++ ext_tables.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') or die();
+
+$lll = 'LLL:EXT:custom_user_settings/Resources/Private/Language/locallang_db.xlf:';
+
+$GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_passionweb_check'] = [
+    'label' => $lll . 'tx_passionweb_check',
+    'type' => 'check',
+    'renderType' => 'checkbox',
+];
+
+$GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_passionweb_contributors'] = [
+    'label' => $lll . 'tx_passionweb_contributors',
+    'type' => 'text',
+    'table' => 'be_users',
+];
+
+ExtensionManagementUtility::addFieldsToUserSettings(
+    'tx_passionweb_check, tx_passionweb_contributors',
+    'after:email',
+);
+
+
+
